760d0ac5102eee45e618fc3d7a688f27027a6215
emlynjones <emlynjones121@gmail.com>
2016-03-27 16:07:11 +0100

    Newid Steil
    Adio FFontiau a newid steil pennyn/cynnwys

--- functions.php
+++ functions.php
@@ -13,8 +13,29 @@ function theme_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
 function thema_siop_fonts(){
-  wp_enqueue_style( 'thema-siop-google-fonts', 'https://fonts.googleapis.com/css?family=Josefin+Sans|Libre+Baskerville', false );
+  wp_enqueue_style( 'thema-siop-google-fonts', 'https://fonts.googleapis.com/css?family=Playfair+Display|Open+Sans', false );
 }
 
 add_action('wp_enqueue_scripts', 'thema_siop_fonts');
+
+/* WooCommerce */
+
+/**
+ * Llwytho ffeil woocommerce.php
+ */
+ require get_template_directory().'/inc/woocommerce.php';
+
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+add_action('woocommerce_before_main_content', 'thema_cychwyn_tud_wc', 10);
+add_action('woocommerce_after_main_content', 'thema_diwedd_tud_wc', 10);
+
+function thema_cychwyn_tud_wc() {
+  echo '<div id="primary" class="content-area">';
+}
+
+function thema_diwedd_tud_wc() {
+  echo '</div>';
+}
 ?>
